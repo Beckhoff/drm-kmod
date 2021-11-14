b79fddfae4d54b977daa265ad81833aec1f8f6f0
Vladimir Kondratyev <wulf@FreeBSD.org>
2021-11-14 03:49:42 +0300

    LinuxKPI: Implement clflush_cache_range

--- linuxkpi/gplv2/include/linux/page.h
+++ linuxkpi/gplv2/include/linux/page.h
@@ -1,4 +1,28 @@
+#ifndef _GPLV2_LINUX_PAGE_H_
+#define	_GPLV2_LINUX_PAGE_H_
+
 #include_next <linux/page.h>
+
+#if defined(__i386__) || defined(__amd64__)
+#include <machine/md_var.h>
+
 #ifndef clflush
 #define	clflush(addr)	clflush((unsigned long)(addr))
 #endif
+
+static inline void
+__clflush_cache_range(void *vaddr, unsigned int size)
+{
+	void *p;
+
+	mb();
+	for (p = (void *)((unsigned long)vaddr & ~(cpu_clflush_line_size - 1));
+	     p < vaddr + size;
+	     p += cpu_clflush_line_size)
+		clflushopt(p);
+	mb();
+}
+#define	clflush_cache_range(vaddr, size) __clflush_cache_range(vaddr, size)
+#endif
+
+#endif	/* _GPLV2_LINUX_PAGE_H_ */
